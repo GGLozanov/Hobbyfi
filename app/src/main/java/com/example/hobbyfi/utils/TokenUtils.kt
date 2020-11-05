8605952d784f36c7f51685d6080fd5da181753ba
GGLozanov <glozanov15@gmail.com>
2020-11-05 21:21:35 +0200

    [Update] Added empty nav graphs & small TODOs

--- app/src/main/java/com/example/hobbyfi/utils/TokenUtils.kt
+++ app/src/main/java/com/example/hobbyfi/utils/TokenUtils.kt
@@ -1,4 +1,5 @@
 package com.example.hobbyfi.utils
 
 class TokenUtils {
+    // TODO: JWT and Facebook access token utils go here
 }
--- app/src/main/java/com/example/hobbyfi/viewmodels/auth/AuthFragmentViewModel.kt
+++ app/src/main/java/com/example/hobbyfi/viewmodels/auth/AuthFragmentViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.hobbyfi.viewmodels.auth
 
 import android.app.Application
+import android.provider.Contacts.PresenceColumns.IDLE
+import com.example.hobbyfi.state.TokenState
 import com.example.hobbyfi.viewmodels.base.BaseViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 
 abstract class AuthFragmentViewModel(application: Application) : BaseViewModel(application) {
+    @ExperimentalCoroutinesApi
+    private val tokenState: MutableStateFlow<TokenState> = MutableStateFlow(TokenState.Idle)
 }
