d5ecf37482e57bb2ce5902443fe9c83d492cb136
GGLozanov <glozanov15@gmail.com>
2020-12-09 20:26:59 +0200

    [Update] Abstracted Create screens from Activities to Fragments

---
+++ app/src/main/java/com/example/hobbyfi/viewmodels/create/ChatroomCreateFragmentViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.hobbyfi.viewmodels.create
+
+import android.app.Application
+import androidx.databinding.Bindable
+import androidx.lifecycle.MutableLiveData
+import androidx.multidex.MultiDexApplication
+import com.example.hobbyfi.viewmodels.base.BaseViewModel
+import com.example.hobbyfi.viewmodels.base.TwoWayDataBindable
+import com.example.hobbyfi.viewmodels.base.TwoWayDataBindableViewModel
+
+class ChatroomCreateActivityViewModel(application: Application) : BaseViewModel(application),
+    TwoWayDataBindable by TwoWayDataBindableViewModel() {
+    // TODO: Extract TwoWayDataBindable + username + description + tags (maybe) in interface + class delegate for two-way data binding
+    @Bindable
+    val name: MutableLiveData<String> = MutableLiveData()
+
+    @Bindable
+    val description: MutableLiveData<String> = MutableLiveData()
+}
