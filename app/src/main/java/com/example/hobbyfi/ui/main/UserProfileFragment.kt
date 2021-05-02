cd4beb7adf357c9178c1f1f79198866efaf82b1e
GGLozanov <glozanov15@gmail.com>
2021-05-03 00:15:53 +0300

    [Update] Attempts at theming (w/ a seductive inclination!)

--- app/src/main/java/com/example/hobbyfi/ui/main/UserProfileFragment.kt
+++ app/src/main/java/com/example/hobbyfi/ui/main/UserProfileFragment.kt
@@ -87,6 +87,11 @@ class UserProfileFragment : MainFragment(), TextFieldInputValidationOnus {
                 )
             }
 
+            askSwitch.setOnCheckedChangeListener { _, checked ->
+                prefConfig.writeChatroomJoinRememberNavigate(if(checked) Constants.NoRememberDualChoice.REMEMBER_YES.ordinal
+                    else Constants.NoRememberDualChoice.NO_REMEMBER.ordinal)
+            }
+
             return@onCreateView root
         }
     }
